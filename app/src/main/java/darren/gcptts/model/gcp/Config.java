5dd6a4385f201ed2dcd004fbc0b7e36f21b7e718
Cicci Miranda <cicci.miranda@pdsglobal.com>
2019-12-02 14:31:40 +0800

    remove api key

--- app/src/main/java/darren/gcptts/model/gcp/Config.java
+++ app/src/main/java/darren/gcptts/model/gcp/Config.java
@@ -7,7 +7,7 @@ package darren.gcptts.model.gcp;
  * Reference:
  */
 class Config {
-    static final String API_KEY = "AIzaSyCURR9m3vZz44cBAoiJZCORyN8I1yPZ1dY"; // put your key to here
+    static final String API_KEY = ""; // put your key to here
     static final String API_KEY_HEADER = "X-Goog-Api-Key";
     static final String SYNTHESIZE_ENDPOINT = "https://texttospeech.googleapis.com/v1beta1/text:synthesize";
     static final String VOICES_ENDPOINT = "https://texttospeech.googleapis.com/v1beta1/voices";
